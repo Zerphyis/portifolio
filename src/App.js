c5cc486994036a3ad304f96f6c29288a4da30c35
Otavio <estudosprogotavio@gmail.com>
2025-06-20 15:41:48 -0300

    refactor:adicionado o app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import SectionSkills from "./components/section-skills/SectionSkills";
+import ProjectSection from "./components/project-section/ProjectSection";
+import ContactSection from "./components/contact-section/ContactSection";
+import Footer from "./components/footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <SectionSkills />
+        <ProjectSection />
+        <ContactSection />
+      </main>
+      <Footer />
     </div>
   );
 }
